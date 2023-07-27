3cbfd75461072996c7bca7e50934c364bf77e6d1
mdmarufsarker <mdmarufsarker2021@gmail.com>
2023-07-27 17:20:48 +0600

    start from works section

--- src/components/Experience/ExperienceSection.tsx
+++ src/components/Experience/ExperienceSection.tsx
@@ -7,6 +7,19 @@ export default function ExperienceSection() {
       className="max-w-containerContent mx-auto py-10 lgl:py-24 px-4"
     >
       <SectionTitle titleNo="02" title="Where I have worked" />
+      <div className="w-full mt-10 flex flex-col md:flex-row gap-16">
+        <ul className="md:w-32 flex flex-col">
+          <li className="border-1-2 border-1-hoverColor bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">
+            Stack Learner
+          </li>
+          <li className="border-1-2 border-1-hoverColor bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">
+            Beecrowd
+          </li>
+          <li className="border-1-2 border-1-hoverColor bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium ">
+            CPS Academy
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
---
+++ src/components/Works/Beecrowd.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Beecrowd() {
+  return (
+    <div>Beecrowd</div>
+  )
+}
